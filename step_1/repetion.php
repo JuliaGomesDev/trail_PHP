3cc5c0f12b3e621047193e8a78a7a674e2d055f9
julia_gomes <julia.gomes@novahaus.com.br>
2023-02-27 15:12:27 -0300

    refactor: add loop for on the repetion.php

--- step_1/repetion.php
+++ step_1/repetion.php
@@ -1,8 +1,12 @@
 <?php
 
-$id = 1;
+$id = 16;
 
 while ($id <= 15) {
   echo "#$id" . PHP_EOL;
   $id = $id + 1;
+}
+
+for ($order = 0; $order <= 10; $order++) {
+  echo "O pedido $order está pronto" . PHP_EOL;
 }
