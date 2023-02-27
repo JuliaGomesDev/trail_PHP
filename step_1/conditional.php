53bbc460586f65bcc8a2a0806718ada3cc2cbe19
julia_gomes <julia.gomes@novahaus.com.br>
2023-02-27 14:16:26 -0300

    modified: conditional.php

--- step_1/conditional.php
+++ step_1/conditional.php
@@ -1,11 +1,17 @@
 <?php
 
-$age = 18;
+$age = 17;
+$numberOfPersons = 2;
 
-echo "Você só pode entrar se tiver a partir de 18 anos" . PHP_EOL;
+echo "Você só pode entrar se tiver a partir de 18 anos ou "; 
+echo "a partir de 16 anos acompanhado". PHP_EOL;
 
 if ($age >= 18) {
   echo "Você tem $age anos. Pode entrar\n";
 } else {
-  echo "Você é menor de idade. Não pode entrar\n";
+    if ($age > 16 && $numberOfPersons > 1) {
+      echo "Você tem $age anos e está acompanhado. Pode entrar\n";
+    } else {
+      echo "Você não atende aos requisitos para entrar\n";
+    }
 }
