8470f720eeff86a2b73a452ec67b5caa47c6b4be
julia_gomes <julia.gomes@novahaus.com.br>
2023-02-27 14:02:46 -0300

    modified: conditional.php

--- step_1/conditional.php
+++ step_1/conditional.php
@@ -1,2 +1,11 @@
 <?php
 
+$age = 18;
+
+echo "Você só pode entrar se tiver mais de 18 anos" . PHP_EOL;
+
+if ($age >= 18) {
+  echo "Você tem $age anos. Pode entrar\n";
+} else {
+  echo "Você é menor de idade. Não pode entrar\n";
+}
