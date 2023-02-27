6ab80930b101f0eefb4f1c9560960b9d37f7d462
julia_gomes <julia.gomes@novahaus.com.br>
2023-02-27 14:57:28 -0300

    refactor: add a new option of conditional with ternary

--- step_1/conditional.php
+++ step_1/conditional.php
@@ -1,6 +1,6 @@
 <?php
 
-$age = 17;
+$age = 18;
 $numberOfPersons = 2;
 
 echo "Você só pode entrar se tiver a partir de 18 anos ou "; 
@@ -8,10 +8,13 @@ echo "a partir de 16 anos acompanhado". PHP_EOL;
 
 if ($age >= 18) {
   echo "Você tem $age anos. Pode entrar\n";
+} else if ($age > 16 && $numberOfPersons > 1) {
+  echo "Você tem $age anos e está acompanhado(a). Pode entrar\n";
 } else {
-    if ($age > 16 && $numberOfPersons > 1) {
-      echo "Você tem $age anos e está acompanhado. Pode entrar\n";
-    } else {
-      echo "Você não atende aos requisitos para entrar\n";
-    }
+  echo "Você tem $age e está sozinho. Não pode entrar\n";
 }
+
+$nationality = 'brasileiro';
+$message = $nationality == 'brasileiro' ? "Você é $nationality. Pode entrar\n" : "Você é $nationality. Não pode entrar" . PHP_EOL;
+
+echo $message;
