8758457818b44026806cdb6249468d9c0d583bdd
julia_gomes <julia.gomes@novahaus.com.br>
2023-03-02 18:16:21 -0300

    feat: adding html inside bank.php's file

--- step_1/advanced/bank.php
+++ step_1/advanced/bank.php
@@ -25,7 +25,30 @@ nameUpperCase($accounts['123.456.789-10']);
 
 unset($accounts['123.456.789-11']);
 
-foreach ($accounts as $cpf => $account) {
-  ['owner' => $owner, 'balance' => $balance] = $account;
-  printMessage("$cpf - $owner - $balance");
-}
+?>
+
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+  <meta charset="UTF-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>PHP e HTML</title>
+</head>
+<body>
+  <h1>Contas correntes</h1>
+  <?php foreach($accounts as $cpf => $account) { ?>
+    <dl>
+      <dt>
+        <strong>Cliente: <?= $account['owner'] ?> </strong> 
+      </dt>
+      <dd>
+        CPF: <?= $cpf ?>
+      </dd>
+      <dd>
+        Saldo: <?= $account['balance'] ?>
+      </dd>
+    </dl>
+  <?php } ?>  
+</body>
+</html>
