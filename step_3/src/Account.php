83b9bc8962c51e130ac7ff2f9084126f80fc366f
julia_gomes <julia.gomes@novahaus.com.br>
2023-03-16 18:48:11 -0300

    feat: add file bank.php

--- step_3/src/Account.php
+++ step_3/src/Account.php
@@ -1,9 +1,64 @@
 <?php
 class Account 
 {
-  public string $ownerCPF;
-  public string $ownerName;
-  public float $balance = 0;
+  private string $ownerCPF = '';
+  private string $ownerName = '';
+  private float $balance = 0;
+
+  /**
+   * Get the value of ownerCPF
+   */
+  public function getOwnerCPF(): string
+  {
+    return $this->ownerCPF;
+  }
+
+  /**
+   * Set the value of ownerCPF
+   */
+  public function setOwnerCPF(string $ownerCPF): self
+  {
+    $this->ownerCPF = $ownerCPF;
+
+    return $this;
+  }
+
+  /**
+   * Get the value of ownerName
+   */
+  public function getOwnerName(): string
+  {
+    return $this->ownerName;
+  }
+
+  /**
+   * Set the value of ownerName
+   */
+  public function setOwnerName(string $ownerName): self
+  {
+    $this->ownerName = $ownerName;
+
+    return $this;
+  }
+
+  /**
+   * Get the value of balance
+   */
+  public function getBalance(): float
+  {
+    return $this->balance;
+  }
+
+  /**
+   * Set the value of balance
+   */
+  public function setBalance(float $balance): self
+  {
+    $this->balance = $balance;
+
+    return $this;
+  }
+
   public function toWithdrawn(float $value) : void
   {
     if($this->balance < $value){
@@ -34,39 +89,6 @@ class Account
     $destinyAccount->toDeposit($valueToTransfer);
   }
 };
-
-# Testing functions
-
-$conta1 = new Account();
-var_dump($conta1);
-
-$conta1->balance = 500;
-var_dump($conta1);
-
-$conta1->toWithdrawn(600);
-$conta1->toWithdrawn(100);
-var_dump($conta1);
-
-$conta1->toDeposit(-100);
-$conta1->toDeposit(200);
-var_dump($conta1);
-
-$conta2 = new Account();
-
-$conta1->transfer(800, $conta2);
-$conta1->transfer(200, $conta2);
-var_dump($conta1);
-var_dump($conta2);
-
-$conta1->ownerName = 'Júlia';
-$lastname = ' Gomes';
-echo $conta1->ownerName .= $lastname . PHP_EOL;
-
-echo $conta1->ownerCPF ?? 'CPF não informado' . PHP_EOL;
-
-$conta1->ownerCPF = '123.456.789-10';
-
-echo $conta1->ownerCPF ?? 'CPF não informado' . PHP_EOL;
 
 ?>
 
---
+++ step_3/src/bank.php
@@ -0,0 +1,49 @@
+<?php 
+
+require_once 'Account.php';
+function validateCPF(Account $account) : bool|string
+{
+  if($account->getOwnerCPF() == '') {
+    return false;
+  }
+
+  return $account->getOwnerCPF();
+};
+
+$conta1 = new Account();
+var_dump($conta1);
+
+$conta1->toDeposit(500);
+var_dump($conta1);
+
+$conta1->toWithdrawn(600);
+$conta1->toWithdrawn(100);
+var_dump($conta1);
+
+$conta1->toDeposit(-100);
+$conta1->toDeposit(200);
+var_dump($conta1);
+
+$conta2 = new Account();
+
+$conta1->transfer(800, $conta2);
+$conta1->transfer(200, $conta2);
+var_dump($conta1);
+var_dump($conta2);
+
+$conta1->setOwnerName('Júlia');
+$name = $conta1->getOwnerName();
+$lastname = ' Gomes';
+echo $name .= $lastname . PHP_EOL;
+
+$cpf = validateCPF($conta1);
+
+if($cpf == false) {
+  echo 'CPF não informado' . PHP_EOL;
+} else {
+  echo $cpf . PHP_EOL;
+}
+
+echo $cpf ?? 'CPF não informado' . PHP_EOL;
+
+?>
