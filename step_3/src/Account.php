5c51cedce5fd0c1bbab6bfd33a59868886e3776e
julia_gomes <julia.gomes@novahaus.com.br>
2023-03-16 16:14:32 -0300

    refactor: add new feature inside Account.php

--- step_3/src/Account.php
+++ step_3/src/Account.php
@@ -4,48 +4,69 @@ class Account
   public string $ownerCPF;
   public string $ownerName;
   public float $balance = 0;
-  public function toWithdrawn(float $value) : void 
+  public function toWithdrawn(float $value) : void
   {
     if($this->balance < $value){
       echo 'Valor não pode ser sacado' . PHP_EOL;
-    } else {
-      $this->balance -= $value;
-    };
+      return;
+    }
+
+    $this->balance -= $value;  
   }
   public function toDeposit(float $value) : void 
   {
     if($value < 0){
       echo 'Valor não pode ser depositado' . PHP_EOL;
-    } else {
-      $this->balance += $value;
-    };
+      return;
+    }
+    
+    $this->balance += $value;
+  }
+
+  public function transfer(float $valueToTransfer, Account $destinyAccount) : void
+  {
+    if($valueToTransfer > $this->balance) {
+      echo 'Valor não pode ser transferido' . PHP_EOL;
+      return;
+    } 
+
+    $this->toWithdrawn($valueToTransfer);
+    $destinyAccount->toDeposit($valueToTransfer);
   }
 };
 
 # Testing functions
 
-$conta = new Account();
-var_dump($conta);
-$conta->balance = 500;
-var_dump($conta);
-$conta->toWithdrawn(600);
-$conta->toWithdrawn(100);
-var_dump($conta);
-$conta->toDeposit(-100);
-$conta->toDeposit(200);
-var_dump($conta);
+$conta1 = new Account();
+var_dump($conta1);
 
-$conta->ownerName = 'Júlia';
+$conta1->balance = 500;
+var_dump($conta1);
+
+$conta1->toWithdrawn(600);
+$conta1->toWithdrawn(100);
+var_dump($conta1);
+
+$conta1->toDeposit(-100);
+$conta1->toDeposit(200);
+var_dump($conta1);
+
+$conta2 = new Account();
+
+$conta1->transfer(800, $conta2);
+$conta1->transfer(200, $conta2);
+var_dump($conta1);
+var_dump($conta2);
+
+$conta1->ownerName = 'Júlia';
 $lastname = ' Gomes';
-$conta->ownerName .= $lastname;
+echo $conta1->ownerName .= $lastname . PHP_EOL;
 
-var_dump($conta->ownerName) . PHP_EOL;
+echo $conta1->ownerCPF ?? 'CPF não informado' . PHP_EOL;
 
-echo $conta->ownerCPF ?? 'CPF não informado' . PHP_EOL;
+$conta1->ownerCPF = '123.456.789-10';
 
-$conta->ownerCPF = '123.456.789-10';
-
-echo $conta->ownerCPF ?? 'CPF não informado' . PHP_EOL;
+echo $conta1->ownerCPF ?? 'CPF não informado' . PHP_EOL;
 
 ?>
 
