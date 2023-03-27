4c6213ad30bcb527caf4b3b338be0faaff7ac5fc
julia_gomes <julia.gomes@novahaus.com.br>
2023-03-27 17:51:09 -0300

    feat: add dependences of project - composer.json

---
+++ formation-02/SearchEngine/search-courses.php
@@ -0,0 +1,28 @@
+<?php
+
+require_once 'vendor/autoload.php';
+
+use GuzzleHttp\Client;
+use Symfony\Component\DomCrawler\Crawler;
+
+$client = new Client();
+$response = $client->request('GET', 'https://www.alura.com.br/cursos-online-programacao/php');
+
+$html = $response->getBody();
+
+$crawler = new Crawler();
+$crawler->addHtmlContent($html);
+
+$courses = $crawler->filter('span.card-curso__nome');
+
+foreach ($course as $course) {
+  echo $course->textContent . PHP_EOL;
+}
+
+
+
+
+
+
+
+?>
